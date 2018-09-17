24ff55819f452fd3f549f89a5e163b41fa4037db
pmoya_in_the_web <43010860+pmoya-in-the-web@users.noreply.github.com>
2018-09-17 20:04:20 +0200

    renamed variables with _

--- resilio-uar-uninstall.sh
+++ resilio-uar-uninstall.sh
@@ -2,15 +2,15 @@
 # script ejemplo https://github.com/duckinator/signal-desktop-rpm/blob/master/build-rpm.sh
 
 
-RESILIO_USER_HOME_DIR_CONFIG=$RESILIO_USER_HOME_DIR'/.config/resilio-sync'
-RESILIO_CONFIG_DIR='/etc/resilio-sync'
-RESILIO_SERVICE_DIR='/lib/systemd/system'
-RESILIO_USER=rslsync
-RESILIO_GROUP=rslsync
+_RESILIO_USER_HOME_DIR_CONFIG=$_RESILIO_USER_HOME_DIR'/.config/resilio-sync'
+_RESILIO_CONFIG_DIR='/etc/resilio-sync'
+_RESILIO_SERVICE_DIR='/lib/systemd/system'
+_RESILIO_USER=rslsync
+_RESILIO_GROUP=rslsync
 
 
 
-echo '- Removing resilio-sync service'
+echo 'Removing resilio-sync service'
 ( set -x;
   # Stop service
   systemctl stop resilio-sync
@@ -31,14 +31,14 @@ echo '- Removing resilio-sync service'
 # ToDo: ojo no me vaya a cargar los ficheros compartidos
 
 # Remove configuration directory
-echo '- Removing '$RESILIO_CONFIG_DIR' directory and service configuration backup files'
-rm -r $RESILIO_CONFIG_DIR
-rm $RESILIO_SERVICE_DIR'/resilio-sync.service.bak'
+echo 'Removing '$_RESILIO_CONFIG_DIR' directory and service configuration backup files'
+rm -r $_RESILIO_CONFIG_DIR
+rm $_RESILIO_SERVICE_DIR'/resilio-sync.service.bak'
 
 # Remove user (remove home dir & force)
-echo '- Removing '$RESILIO_USER' user and its home directory'
-userdel -rf $RESILIO_USER
-echo '- Removing '$RESILIO_GROUP' group'
-groupdel -f $RESILIO_GROUP
+echo 'Removing '$_RESILIO_USER' user and its home directory'
+userdel -rf $_RESILIO_USER
+echo 'Removing '$_RESILIO_GROUP' group'
+groupdel -f $_RESILIO_GROUP
 
 exit 0
